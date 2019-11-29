94b94dd36def040aaab42264e115782e079438bc
Atheer M <43179208+atheer10@users.noreply.github.com>
2019-11-29 23:08:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 var express = require('express')
 var cors = require('cors')
-var bodyParser = require('body-parser')
 var app = express()
 const mongoose = require('mongoose')
 var port = process.env.PORT || 5000
 
-app.use(bodyParser.json())
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 app.use(cors())
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-)
+
 
 const mongoURI = 'mongodb://localhost:27017/mernloginreg'
 
